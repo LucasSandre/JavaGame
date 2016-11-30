4eabc141395924b7e11163b26c56594f2f71ed92
VilePoison <lucasyyz@gmail.com>
2016-11-30 12:49:33 -0500

    Started random item drop points

--- Game/src/dev/lucas/game/Launcher.java
+++
@@ -1,9 +0,0 @@
-package dev.lucas.game;
-
-public class Launcher {
-	public static void main(String[] args) {
-		Game game = new Game("Tile Game!",1024,578);
-		game.start();
-	}
-
-}
--- Game/src/dev/lucas/game/ui/ClickListener.java
+++
@@ -1,6 +0,0 @@
-package dev.lucas.game.ui;
-
-public interface ClickListener {
-	
-	public void onClick();
-}
